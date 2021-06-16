764325fc0e09645c0d0f93bc3602ff227dd3862c
hyx131 <selin.hyx@gmail.com>
2021-06-16 12:21:23 -0400

    textfield - add styled input component using inputBase

---
+++ src/components/text-field.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+
+import { InputBase } from '../base-components'
+
+
+const _textFieldClasses = ({ error }) => ({
+  label: clsx('font-sans text-sm ml-1', { 'text-primary-700': !error, 'text-error': error }),
+  helperText: clsx('font-sans text-xs ml-1', { 'text-primary-700': !error, 'text-error': error }),
+})
+const _inputBaseClasses = ({ focus, error, width, filled }) => ({
+  root: clsx(`${width} rounded-sm p-sm`,
+    { 'border-primary shadow-focused-primary': focus && !error },
+    { 'border-error shadow-focused-error': focus && error },
+    { 'border-primary bg-primary-10': filled },
+  ),
+  input: clsx('outline-none text-primary-700', { 'bg-primary-10': filled }),
+  startIcon: 'mt-0.5 mr-1',
+  endIcon: 'mt-0.5 mx-1',
+})
+
+const TextField  = ({ classes, inputProps, label, helperText, error, onSubmit }) => {
+  const [filled, setFilled] = useState(false)
+  const [value, setValue] = useState(false)
+  const [focus, setFocus] = useState(false)
+  const { width } = classes
+  const textFieldClasses = _textFieldClasses({ error })
+  const inputBaseClasses = _inputBaseClasses({ focus, error, width, filled })
+
+  const handleChange = (val) => {
+    setValue(val)
+    if (inputProps.onChange) {
+      inputProps.onChange(val)
+    }
+  }
+  const handleFocus = () => {
+    setFocus(true)
+    setFilled(false)
+  }
+  const handleBlur = () => {
+    setFocus(false)
+    if (value) setFilled(true)
+  }
+
+  return (
+    <div className='flex flex-col'>
+      {label && <p className={textFieldClasses.label}>{label}</p>}
+      <form onSubmit={onSubmit}>
+        <InputBase
+          {...inputProps}
+          classes={inputBaseClasses}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      </form>
+      {helperText && <p className={textFieldClasses.helperText}>{helperText}</p>}
+    </div>
+  )
+}
+
+TextField.propTypes = {
+  classes: PropTypes.object,
+  inputProps: PropTypes.object,
+  label: PropTypes.string,
+  helperText: PropTypes.string,
+  error: PropTypes.bool,
+  onSubmit: PropTypes.func,
+}
+TextField.defaultProps = {
+  classes: { width: '' },
+  inputProps: {},
+  label: '',
+  helperText: '',
+  error: false,
+  onSubmit: () => {},
+}
+
+export default TextField
--- stories/textfield.stories.js
+++ stories/textfield.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { InputBase } from '../src/base-components'
+import { TextField } from '../src'
 import { ChevronUp, ChevronDown } from '../src/icons'
 
 
@@ -9,10 +10,14 @@ export default {
   component: InputBase,
 }
 
-/** -- props:
+const labelClass = 'text-blue-300 mt-5 mb-1'
+
+/** -- props (InputBase):
  * [classes] - object, custom styling supported keys:
  *    root: input container div
  *    input: input element
+ *    startIcon: startIcon container div
+ *    endIcon: endIcon container div
  * [value] - string, value of input
  * [defaultValue] - string, value of input on initial render
  * [placeholder] - string, helper value of input if value is empty
@@ -26,16 +31,55 @@ export default {
 export const Base = () => {
   return (
     <>
-      <p>Normal:</p>
+      <p className={labelClass}>Normal:</p>
       <InputBase />
-      <p className='mt-5'>Placeholder:</p>
+      <p className={labelClass}>Placeholder:</p>
       <InputBase placeholder='Is currenlty busy placeholding...' />
-      <p className='mt-5'>Default Value:</p>
+      <p className={labelClass}>Default Value:</p>
       <InputBase defaultValue='Hi there, I am the default value of this input.' />
-      <p className='mt-5'>Start Icon:</p>
+      <p className={labelClass}>Start Icon:</p>
       <InputBase startIcon={<ChevronUp />} placeholder='Is currenlty busy placeholding...' />
-      <p className='mt-5'>End Icon:</p>
+      <p className={labelClass}>End Icon:</p>
       <InputBase endIcon={<ChevronDown />} placeholder='Is currenlty busy placeholding...' />
+    </>
+  )
+}
+
+/** -- props (TextField):
+ * [classes] - object, custom styling supported keys:
+ *    width: width of text field
+ * [inputProps] - object, accepts all InputBase props
+ * [label] - string, label for text field
+ * [helpertText] - string, small text at bottom of text field
+ * [error] - boolean, toggles error mode
+ * [onSubmit] - function, form submit handler
+ */
+
+export const Normal = () => {
+  const inputProps = { autoFocus: true }
+  const classes = { width: 'w-72' }
+  return (
+    <>
+      <p className={labelClass}>Label + Helper Text:</p>
+      <TextField inputProps={inputProps} label='Label:' helperText='I am helper text' />
+      <p className={labelClass}>Error:</p>
+      <TextField error inputProps={inputProps} label='Label:' helperText='Something went wrong' />
+      <p className={labelClass}>Custom Width:</p>
+      <TextField classes={classes} inputProps={inputProps} label='Label:' helperText='I am helper text' />
+      <p className={labelClass}>Start Icon:</p>
+      <TextField
+        classes={classes}
+        inputProps={{ ...inputProps, startIcon: <ChevronDown /> }}
+        label='Label:'
+        helperText='I am helper text'
+      />
+      <p className={labelClass}>End Icon:</p>
+      <TextField
+        classes={classes}
+        inputProps={{ ...inputProps, endIcon: <ChevronDown /> }}
+        label='Label:'
+        helperText='I am helper text'
+      />
     </>
   )
 }

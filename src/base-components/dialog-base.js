7c1852b1bd8d55adb613b8921d9e3458bc30bd63
hyx131 <selin.hyx@gmail.com>
2021-06-17 10:03:15 -0400

    dialog - change supported anchor positions + flat base component folders

--- src/base-components/dialog-base.js
+++ src/base-components/dialog-base.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import clsx from 'clsx'
 
-import { useComponentIsActive } from '../../hooks'
+import { useComponentIsActive } from '../hooks'
 
 
-const _baseClasses = ({ anchor, modal }) => ({
-  root: `flex ${anchor === 'bottom' ? 'flex-col'
-    : anchor === 'top' ? 'flex-col-reverse'
-      : anchor === 'left' ? 'flex-row-reverse'
-        : anchor === 'right' ? 'flex-row'
-          : ''
-  }`,
-  modal: modal
-    ? 'fixed z-20 max-h-full max-w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-auto bg-white'
-    : '',
+const _baseClasses = ({ anchor }) => ({
+  root: clsx('relative inline-flex', {
+    'flex-row': anchor === 'horizontal',
+    'flex-col': anchor === 'vertical',
+  }),
+  dialog: 'absolute',
+  modal: 'fixed z-20 max-h-full max-w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-auto bg-white',
   overlay: 'absolute z-10 inset-0 w-full h-full bg-black bg-opacity-50 transition-opacity duration-200',
 })
 
@@ -37,10 +35,12 @@ const DialogBase = ({ classes, button, children, modal, open, anchor, onClick })
   return (
     <>
       <div ref={ref} className={`${baseClasses.root} ${classes.root}`}>
-        <div className={classes.button} onClick={handleClick}>{button}</div>
-        {_open && <>
-          <div className={`${baseClasses.modal} ${classes.dialog}`}>{children}</div>
-        </>}
+        <span className={classes.button} onClick={handleClick}>{button}</span>
+        {_open &&<span><div className={clsx({
+          [baseClasses.modal]: modal,
+          [`${baseClasses.dialog} ${classes.dialog}`]: !modal,
+        })}
+        >{children}</div></span>}
       </div>
       {_open && modal && <div className={`${baseClasses.overlay} ${classes.overlay}`}></div>}
     </>
@@ -60,7 +60,7 @@ DialogBase.defaultProps = {
   classes: { root: '', button: '', dialog: '' },
   modal: false,
   open: undefined,
-  anchor: 'bottom',
+  anchor: 'vertical',
   onClick: () => {},
 }
 export default DialogBase

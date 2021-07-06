b8bf1e0c192424d63ac7fb5719ebe7e1a4bdb881
kc-leung <chiu-leung@hotmail.com>
2021-07-06 15:47:05 -0400

    pagination - add pagination component logic & initial styling
    pagination - clean up dirty codes

--- stories/pagination.stories.js
+++ stories/pagination.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Layout } from '../src'
 import Pagination from '../src/components/pagination'
@@ -9,16 +9,31 @@ export default {
   component: Pagination,
 }
 
-export const normal = () => {
-  const headerClass = 'bg-primary-200 text-white text-center h-10'
-  const contentClass = 'bg-primary-400 text-white text-center h-40 w-full'
+const exampleItems = [...Array(12).keys()].map(i => ({ id: (i+1), name: 'Item ' + (i+1) }));
+
+export const Normal = () => {
+  const [item, setItem] = useState({
+    exampleItems: exampleItems,
+    pageOfItems: []
+  })
+
+  const headerClass = 'bg-primary-200 text-white text-center h-full'
+  const contentClass = 'bg-primary-400 text-white text-center h-full w-full'
+
+  const onChangePage = (pageOfItems) => {
+    setItem({ ...item, pageOfItems: pageOfItems });
+  }
 
   return (
     <Layout>
       <Layout.Header className={headerClass}>Header</Layout.Header>
-      <Layout.Content className={contentClass}>Content</Layout.Content>
+      <Layout.Content className={contentClass}>
+        {item.pageOfItems.map(item =>
+          <div key={item.id}>{item.name}</div>
+        )}
+      </Layout.Content>
       <Layout.Footer className={headerClass}>
-        <Pagination />
+        <Pagination items={item.exampleItems} onChangePage={onChangePage}/>
       </Layout.Footer>
     </Layout>
   )

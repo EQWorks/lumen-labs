4c0f882bff29f071e394b8617828a64dfc997a79
kc-leung <chiu-leung@hotmail.com>
2022-01-17 10:48:13 -0500

    customScroll - add lumen predefined color option

--- src/utils/custom-scroll.js
+++ src/utils/custom-scroll.js
@@ -1,12 +1,16 @@
+import { getTailwindConfigColor } from '..'
 import { makeStyles } from  './make-styles'
 
 /**
+ * @param {string} lumenColor use lumen predefined color rules, default = secondary
  * @param {string} width scrollbar width, default = '0.1875rem'. 
- * @param {string - Hex Color} thumbColor scrollbar thumb color, default = '#c8c8c8'.
- * @param {string - Hex Color} trackColor scrollbar track color, default = '#f2f2f2'.
+ * @param {string - Hex Color} thumbColor scrollbar thumb color'.
+ * @param {string - Hex Color} trackColor scrollbar track color'.
+ * @param {string - Hex Color} hoverThumbColor scrollbar thumb color onHover'.
+ * @param {string - Hex Color} hoverTrackColor scrollbar track color on Hover'.
  */
 
-export const customScroll = (width = '0.1875rem', thumbColor = '#c8c8c8', trackColor = '#f2f2f2') => {
+export const customScroll = (lumenColor = 'secondary', width = '0.1875rem', thumbColor, trackColor, hoverThumbColor, hoverTrackColor ) => {
   const classes = makeStyles({
     CustomScroll: {
       scrollbarWidth: 'thin',
@@ -18,21 +22,21 @@ export const customScroll = (width = '0.1875rem', thumbColor = '#c8c8c8', trackC
       },
 
       '&:hover::-webkit-scrollbar-thumb': {
-        backgroundColor: thumbColor,
+        backgroundColor: thumbColor ? thumbColor : getTailwindConfigColor(`${lumenColor}-400`),
         borderRadius: '0.25rem',
       },
 
       '&:hover::-webkit-scrollbar-thumb:hover': {
-        backgroundColor: '#AAAAAA',
+        backgroundColor: hoverThumbColor ? hoverThumbColor : getTailwindConfigColor(`${lumenColor}-500`),
         borderRadius: '0.25rem',
       },
 
       '&:hover::-webkit-scrollbar-track': {
-        backgroundColor: trackColor,
+        backgroundColor: trackColor ? trackColor : getTailwindConfigColor(`${lumenColor}-200`),
       },
 
       '&:hover::-webkit-scrollbar-track:hover': {
-        backgroundColor: '#EAEAEA',
+        backgroundColor: hoverTrackColor ? hoverTrackColor : getTailwindConfigColor(`${lumenColor}-300`),
       },
     },
   })

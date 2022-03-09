3e7d4c3846c384570bd3bb9c4c6a03ee8db1e7cb
hyx131 <selin.hyx@gmail.com>
2022-03-09 14:32:28 -0500

    icons/stories - sort icons alphabetically

--- stories/style-guides/icons.stories.js
+++ stories/style-guides/icons.stories.js
@@ -10,6 +10,34 @@ export default {
   title: 'Style Guide/Icons',
 }
 
+const formatIcons = (filteredIcons) => (
+  Object.entries(filteredIcons).sort().reduce((acc, [name, Icon]) => {
+    const alph = name[0].toUpperCase()
+    if (acc[alph]) {
+      acc[alph] = { ...acc[alph], [name]: Icon }
+    } else {
+      acc[alph] = { [name]: Icon }
+    }
+    return acc
+  }, {}))
+
+const renderIcons = (icons) => (
+  <div className='grid grid-cols-6'>
+    {Object.entries(icons).map(([name, Icon], i) => {
+      const bg = i%2 ? 'bg-primary-200' : 'bg-primary-100'
+      const colorConfigs = ['RightJoin', 'LeftJoin', 'InnerJoin', 'Copy'].includes(name)
+        ? { fill: '#808080', stroke: '#4C4C4C' }
+        : {}
+      return (
+        <div key={i} className={`flex flex-col justify-center items-center mb-7 p-2 ${bg}`}>
+          <Icon size='lg' {...colorConfigs} />
+          <h6 className='text-secondary-700 mt-2'>{name}</h6>
+        </div>
+      )
+    })}
+  </div>
+)
+
 export const Icons = () => {
   const [value, setValue] = useState('')
   const [filteredIcons, setFilteredIcons] = useState({})
@@ -36,19 +64,13 @@ export const Icons = () => {
         inputProps={{ placeholder: 'Icon name', endIcon: <icons.Search size='md' /> }}
         onChange={(val) => setValue(val)}
       />
-      <div className='grid grid-cols-6'>
-        {Object.entries(filteredIcons).map(([name, Icon], i) => {
-          const bg = i%2 ? 'bg-primary-200' : 'bg-primary-100'
-          const colorConfigs = ['RightJoin', 'LeftJoin', 'InnerJoin', 'Copy'].includes(name)
-            ? { fill: '#808080', stroke: '#4C4C4C' }
-            : {}
-          return (
-            <div key={i} className={`flex flex-col justify-center items-center mb-7 p-2 ${bg}`}>
-              <Icon size='lg' {...colorConfigs} />
-              <h6 className='text-secondary-700 mt-2'>{name}</h6>
-            </div>
-          )
-        })}
+      <div className='w-full inline-flex flex-col'>
+        {Object.entries(formatIcons(filteredIcons)).map(([alph, icons], i) => (
+          <div key={`${alph}-${i}`}>
+            <p>{alph}:</p>
+            {renderIcons(icons)}
+          </div>
+        ))}
       </div>
     </>
   )

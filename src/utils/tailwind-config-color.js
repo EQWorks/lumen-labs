694d0dd09d88a3c0a4426834b9c6b8685f9ff8cd
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-03-09 20:35:21 -0500

    tailwind - store all colors as independent constants

--- src/utils/tailwind-config-color.js
+++ src/utils/tailwind-config-color.js
@@ -1,11 +1,8 @@
-import tailwindConfig from '../../tailwind.config'
+import { tailwindThemeColors, tailwindExtendColors } from '../../colors'
 
 // Access theme color values for dynamically applying inline styles in React
 export const getTailwindConfigColor = (color) => {
-  const allColors = {
-    ...tailwindConfig.theme.colors,
-    ...tailwindConfig.theme.extend.colors,
-  }
+  const allColors = { ...tailwindThemeColors, ...tailwindExtendColors }
   return color.split('-').length > 1
     ? allColors[color]
     : ''

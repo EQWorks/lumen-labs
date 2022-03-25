061f0a65e4ee50629e153576bfba18074c3edf1f
hyx131 <selin.hyx@gmail.com>
2022-03-25 09:41:14 -0400

    progress/indicator - add stories

--- stories/progress-indicator.stories.js
+++ stories/progress-indicator.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { ProgressIndicator } from '../src'
 
@@ -16,5 +16,32 @@ export const Normal = () => {
     { label: 'testing interface', active: false, complete: false },
     { label: 'finalizing', active: false, complete: false },
   ]
+  return (<ProgressIndicator indicators={indicators} />)
+}
+
+const defaultIndicators = [
+  { label: 'building dashboard', active: true, complete: false },
+  { label: 'loading reports', active: false, complete: false },
+  { label: 'adding data', active: false, complete: false },
+  { label: 'testing interface', active: false, complete: false },
+  { label: 'finalizing', active: false, complete: false },
+]
+export const WithAction = () => {
+  const [indicators, setIndicators] = useState(defaultIndicators)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIndicators((prev) => {
+        const prevIndicators = [...prev]
+        const index = prev.findIndex(({ complete }) => !complete)
+        if (index > -1) {
+          prevIndicators.splice(index, 1, { ...prev[index], complete: true })
+          return prevIndicators
+        }
+        return defaultIndicators
+      })
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [indicators])
+
   return (<ProgressIndicator indicators={indicators} />)
 }

72eb3ae26471836d763bd280cea6904504b4619e
hyx131 <selin.hyx@gmail.com>
2022-03-28 10:24:38 -0400

    common-components/login - mock email submit step
    - remove local logo image file
    - rename "common-components" classification to "modules"

--- stories/common-components/login.stories.js
+++ stories/common-components/login.stories.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import Login from '../../src/common-components/login'
-import LOGO from './images/secondary_logo.png'
-
+import Login from '../../src/modules/login'
 
 export default {
-  title: 'Common Components/Login',
+  title: 'Modules/Login',
 }
 
 export const Normal = () => {
+  const [loadingConfig, setLoadingConfig] = useState({})
+
+  const handleEmailSubmit = () => {
+    setLoadingConfig((prev) => ({ ...prev, emailLoading: true, emailLoadingMsg: 'Sending Authentication Code' }))
+    setTimeout(() => {
+      setLoadingConfig((prev) => ({ ...prev, emailLoading: false, emailLoadingMsg: '' }))
+    }, 2000)
+  }
+
   return (
     <Login
       product='Power Report Dashboard'
-      logo={<img width='114px' height='64px' src={LOGO} alt='logo' />}
+      logo={<h1 className='uppercase text-5xl text-secondary-50'>logo</h1>}
+      loadingConfig={loadingConfig}
+      onChange={(val) => {
+        if (loadingConfig?.emailLoading !== val.loadingConfig?.emailLoading || loadingConfig?.passcodeLoading !== val.loadingConfig?.passcodeLoading) {
+          setLoadingConfig(val.loadingConfig)
+        }
+      }}
+      onEmailSubmit={handleEmailSubmit}
+      classes={{ footerLogo: 'bg-secondary-300' }}
     />
   )
 }

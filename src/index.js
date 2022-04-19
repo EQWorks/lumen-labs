4915de9b4bf6962170a73c5be38fbadb4ee004a4
hyx131 <selin.hyx@gmail.com>
2022-04-19 14:07:22 -0400

    message - add Message component + fix Textfiled no value error

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ export { default as Checkbox } from './components/checkbox'
 export { default as RangeSliderLabel } from './components/range-slider-label/'
 export { default as Label } from './components/label'
 export { default as DatePicker } from './components/date-picker'
+export { default as Message } from './components/message'
 
 export { makeStyles, LinearAnimation } from './utils/make-styles'
 export { getTailwindConfigColor } from './utils/tailwind-config-color'

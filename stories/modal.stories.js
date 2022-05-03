d586aaee49fa865f901f0b1c4ee64796aa6f6fa6
kc-leung <chiu-leung@hotmail.com>
2022-05-03 14:12:50 -0400

    modal - hide close modal & custom close example

--- stories/modal.stories.js
+++ stories/modal.stories.js
@@ -59,7 +59,7 @@ export const Base = () => {
  * [open] - boolean, whether the Dialog is open or not.
  * [closeModal] - function, called when the Modal is dismissed (via the overlay or Escape key). Typically used to close the Modal by setting open to false.
  * [size] - string, determines the size of the component by ['lg', 'md', 'sm'] - default 'md'
- * [title] - string, value of title
+ * [hideClose] - boolean, whether the default header close button is hidden or not.
  */
 
 export const Normal = () => {
@@ -108,6 +108,84 @@ export const Normal = () => {
         size={size} 
         open={isOpen} 
         closeModal={onClose} 
+      >
+        <Modal.Header>
+          Modal size - {size.toUpperCase()}
+        </Modal.Header>
+        <Modal.Content>
+          <TextField
+            size='md'
+            inputProps={{ ...inputProps, prefix: 'https://', suffix: '.com' }}
+            label='Email'
+          />
+          <span className='mt-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+        </Modal.Content>
+        <Modal.Footer classes={modalClasses}>
+          {footerContent}
+        </Modal.Footer>
+      </Modal>
+      <div className='flex flex-row'>
+        <div className='mx-5'>
+          <Button size='md' onClick={() => onClick('lg')}>Large Modal</Button>
+        </div>
+        <div className='mx-5'>
+          <Button size='md' onClick={() => onClick('md')}>Medium Modal</Button>
+        </div>
+        <div className='mx-5'>
+          <Button size='md' onClick={() => onClick('sm')}>Small Modal</Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export const CustomClose = () => {
+  const modalClasses = Object.freeze({
+    footer: 'flex justify-between',
+  })
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [size, setSize] = useState('md')
+
+  const inputProps = { placeholder: 'Placeholder text' }
+
+  const onClose = () => {
+    setIsOpen(true)
+  }
+
+  const onClick = (s) => {
+    setIsOpen(!isOpen) 
+    setSize(s)
+  }
+
+  const footerContent = (
+    <>
+      {size === 'sm' ? 
+        <>
+          <Button size={size === 'lg' ? 'lg' : 'md'} onClick={() => onClick('lg')}>Cancel</Button>
+          <Button variant='filled' size={size === 'lg' ? 'lg' : 'md'}>Submit</Button>
+        </>
+        :
+        <>
+          <Button size={size === 'lg' ? 'lg' : 'md'} type='error' onClick={() => onClick('lg')}>Cancel</Button>
+          <div className="right-button flex flex-row">
+            <div className="pr-4">
+              <Button variant='outlined' size={size === 'lg' ? 'lg' : 'md'}>Reset</Button>
+            </div>
+            <Button variant='filled' size={size === 'lg' ? 'lg' : 'md'}>Save Selection</Button>
+          </div>
+        </>
+      }
+    </>
+  )
+  
+  return (
+    <>
+      <Modal 
+        size={size} 
+        open={isOpen} 
+        closeModal={onClose} 
+        hideClose
       >
         <Modal.Header>
           Modal size - {size.toUpperCase()}

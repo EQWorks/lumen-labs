94cf7c655f3fce70846258348af3e24c7473c372
Hamaad Chughtai <Hamaad102@gmail.com>
2023-01-11 13:26:10 -0500

    radio - label can be displayed on either side of button

--- src/components/radio-group.js
+++ src/components/radio-group.js
@@ -2,14 +2,14 @@ import React, { Children, isValidElement, cloneElement } from 'react'
 import PropTypes from 'prop-types'
 
 
-const RadioGroup = ({ children, name, selected, align }) => {
+const RadioGroup = forwardRef(({ children, name, selected, align, labelAlign }, ref) => {
   const flexDir = align === 'vertical' ? 'flex-col' : 'flex-row'
 
   return (
     <div className={`radio-group__root-contaienr inline-flex ${flexDir}`}>
       {Children.map(children, (child) => {
         if (isValidElement(child)) {
-          return cloneElement(child, { name: name, selected: selected, direction: flexDir } )
+          return cloneElement(child, { name: name, selected: selected, direction: flexDir, align: labelAlign } )
         }
         return child
       })}
@@ -22,9 +22,11 @@ RadioGroup.propTypes = {
   name: PropTypes.string.isRequired,
   selected: PropTypes.string.isRequired,
   align: PropTypes.string,
+  labelAlign: PropTypes.string,
 }
 RadioGroup.defaultProps = {
   align: 'vertical',
+  labelAlign: 'right',
 }
 
 export default RadioGroup

d17fe24ba50e9fb2bdf5b86238952818925365ac
Pei-Yi Lin <amy83762100@gmail.com>
2023-04-14 16:23:15 -0400

    components/card - place classes styling before any default ones & add default empty string when one class is not present

--- src/base-components/card-base.js
+++ src/base-components/card-base.js
@@ -6,12 +6,12 @@ import Layout from '../components/layout'
 
 
 const CardBase = ({ classes, header, content, footer, onClose }) => (
-  <div className={`inline-flex flex-col border ${classes.root}`}>
-    {onClose && <Close size='md' className={`self-end cursor-pointer ${classes.closeIcon}`} onClick={onClose} />}
+  <div className={`${classes.root || ''} inline-flex flex-col border`}>
+    {onClose && <Close size='md' className={`${classes.closeIcon} self-end cursor-pointer`} onClick={onClose} />}
     <Layout className='h-full'>
-      {header && <Layout.Header className={classes.header}>{header}</Layout.Header>}
-      <Layout.Content className={`h-full ${classes.content}`}>{content}</Layout.Content>
-      {footer && <Layout.Footer className={classes.footer}>{footer}</Layout.Footer>}
+      {header && <Layout.Header className={classes.header || ''}>{header}</Layout.Header>}
+      <Layout.Content className={`${classes.content || ''} h-full`}>{content}</Layout.Content>
+      {footer && <Layout.Footer className={classes.footer || ''}>{footer}</Layout.Footer>}
     </Layout>
   </div>
 )

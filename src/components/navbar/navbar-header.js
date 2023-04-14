e6d608e9530b7e5e8c79d3ba24a1b962b4572cc0
Hamaad Chughtai <Hamaad102@gmail.com>
2023-04-14 15:46:48 -0400

    components/navbar - created unstyled navbar component

---
+++ src/components/navbar/navbar-header.js
@@ -0,0 +1,40 @@
+import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
+
+
+const NavbarHeader = forwardRef(({  
+  classes, 
+  children,
+  onClick, 
+  ...rest 
+}, ref) => {
+  const navbarHeaderClasses = Object.freeze({
+    headerContainer: `${classes.headerContainer} navbar__header-container flex items-center cursor-pointer`,
+  })
+
+  return (
+    <div 
+      ref={ref}
+      className={navbarHeaderClasses.headerContainer} 
+      onClick={onClick} 
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+})
+
+NavbarHeader.propTypes = {
+  classes: PropTypes.object,
+  children: PropTypes.any.isRequired,
+  onClick: PropTypes.func,
+}
+  
+NavbarHeader.defaultProps = {
+  classes: { headerContainer: '' },
+  onClick: () => {},
+}
+  
+NavbarHeader.displayName = 'NavbarHeader'
+  
+export default NavbarHeader
---
+++ src/components/navbar/navbar-item.js
@@ -0,0 +1,69 @@
+import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
+
+
+const NavbarItem = forwardRef(({ 
+  id, 
+  classes, 
+  label, 
+  icon, 
+  onClick, 
+  isSelected, 
+  ...rest 
+}, ref) => {
+  const NavbarItemClasses = Object.freeze({
+    itemContainer: `${classes.itemContainer} navbar-item__main-container cursor-pointer mx-6`,
+    itemContainerActive: `${classes.itemContainerActive} navbar-item__main-container--active py-2 px-4 mx-2 rounded-full bg-black`,
+    contentContainer: `${classes.contentContainer} navbar-item__content-container flex flex-row items-center`,
+    icon: `${classes.icon} navbar-item__icon pr-2.5`,
+    iconActive: `${classes.iconActive} navbar-item__icon pr-2.5 text-white`,
+    text: `${classes.text} navbar-item__text text-black`,
+    textActive: `${classes.textActive} navbar-item__text text-white`,
+  })
+
+  return (
+    <div 
+      ref={ref}
+      key={id}
+      className={`${isSelected ? NavbarItemClasses.itemContainerActive : NavbarItemClasses.itemContainer}`} 
+      onClick={onClick} 
+      {...rest}
+    >
+      <div className={NavbarItemClasses.contentContainer}>
+        <div className={`${isSelected ? NavbarItemClasses.iconActive : NavbarItemClasses.icon}`}>
+          {React.cloneElement(icon, { ...icon.props })}
+        </div>
+        <div className={`${isSelected ? NavbarItemClasses.textActive : NavbarItemClasses.text}`}>{label}</div>
+      </div>
+    </div>
+  )
+})
+
+NavbarItem.propTypes = {
+  id: PropTypes.string,
+  classes: PropTypes.object,
+  label: PropTypes.string,
+  icon: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  isSelected: PropTypes.bool,
+}
+
+NavbarItem.defaultProps = {
+  id: '',
+  classes: { 
+    itemContainer: '', 
+    itemContainerActive: '', 
+    contentContainer: '', 
+    icon: '',
+    iconActive: '', 
+    text: '',
+    textActive: '',
+  },
+  label: '',
+  onClick: () => {},
+  isSelected: false,
+}
+
+NavbarItem.displayName = 'NavbarItem'
+
+export default NavbarItem

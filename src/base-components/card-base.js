b0e9f0acb0ceb8021a67565d01b2d0718a7b975e
Pei-Yi Lin <amy83762100@gmail.com>
2023-04-17 12:48:04 -0400

    components/cardBase - make classes prop as required and remove empty string default value

--- src/base-components/card-base.js
+++ src/base-components/card-base.js
@@ -6,19 +6,19 @@ import Layout from '../components/layout'
 
 
 const CardBase = ({ classes, header, content, footer, onClose }) => (
-  <div className={`${classes.root || ''} inline-flex flex-col border`}>
+  <div className={`${classes.root} inline-flex flex-col border`}>
     {onClose && <Close size='md' className={`${classes.closeIcon} self-end cursor-pointer`} onClick={onClose} />}
     <Layout className='h-full'>
-      {header && <Layout.Header className={classes.header || ''}>{header}</Layout.Header>}
-      <Layout.Content className={`${classes.content || ''} h-full`}>{content}</Layout.Content>
-      {footer && <Layout.Footer className={classes.footer || ''}>{footer}</Layout.Footer>}
+      {header && <Layout.Header className={classes.header}>{header}</Layout.Header>}
+      <Layout.Content className={`${classes.content} h-full`}>{content}</Layout.Content>
+      {footer && <Layout.Footer className={classes.footer}>{footer}</Layout.Footer>}
     </Layout>
   </div>
 )
 
 CardBase.propTypes = {
   content: PropTypes.any.isRequired,
-  classes: PropTypes.object,
+  classes: PropTypes.object.isRequired,
   header: PropTypes.any,
   footer: PropTypes.any,
   onClose: PropTypes.func,

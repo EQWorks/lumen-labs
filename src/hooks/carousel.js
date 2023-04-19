300a9b07b6f950f87c837895b7f57dce13fbab6f
kc-leung <chiu-leung@hotmail.com>
2023-04-19 14:58:59 -0400

    carousel - add hook to handle carousel logic for single and multi variant

--- src/hooks/carousel.js
+++ src/hooks/carousel.js
@@ -1,7 +1,7 @@
 import { useCallback, useState, useEffect } from 'react'
 
 
-export const useCarousel = (ref) => {
+export const useFeaturedCarousel = (ref) => {
   const [prevElement, setPrevElement] = useState(null)
   const [nextElement, setNextElement] = useState(null)
 
@@ -74,4 +74,71 @@ export const useCarousel = (ref) => {
   ])
 
   return { scrollRight, scrollLeft }
+}
+
+const getContainerMargin = (element, marginSide) => {
+  const getStyle = window.getComputedStyle(element)
+  return Number(getStyle[marginSide].replace(/\D/g, ''))
+}
+
+export const useCarousel = (carouselRef, variant, length) => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const movePrev = () => {
+    if (variant === 'multi' && carouselRef.current !== null) {
+      if (currentIndex > 0) {
+        setCurrentIndex((prevState) => prevState - 1)
+      }
+    }
+    else if (variant === 'single' && carouselRef.current !== null && currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1)
+    }
+  }
+
+  const moveNext = () => {
+    if (variant === 'multi') {
+      if (
+        carouselRef.current !== null && currentIndex < Math.ceil((length / Math.floor(carouselRef.current.offsetWidth / 
+          carouselRef.current.childNodes[0].clientWidth) - 1))
+      ) {
+        setCurrentIndex((prevState) => prevState + 1)
+      }
+    } 
+    else if (variant === 'single' && carouselRef.current !== null && currentIndex < length - 1) {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
+  const isDisabled = (direction) => {
+    if (direction === 'prev') {
+      return currentIndex <= 0
+    }
+
+    if (direction === 'next' && carouselRef.current !== null) {
+      if (variant === 'multi') {
+        return currentIndex >= Math.ceil((length / Math.floor(carouselRef.current.offsetWidth / carouselRef.current.childNodes[0].clientWidth) - 1))
+      }
+      else if (variant === 'single') {
+        return currentIndex === length - 1
+      }
+    }
+
+    return false
+  }
+
+  useEffect(() => {
+    if (carouselRef !== null && carouselRef?.current !== null) {
+      const getMarginRight = getContainerMargin(carouselRef.current.childNodes[0], 'marginRight')
+      const getMarginLeft = getContainerMargin(carouselRef.current.childNodes[0], 'marginLeft')
+      const visibleItems = Math.floor(carouselRef.current.offsetWidth / carouselRef.current.childNodes[0].clientWidth)
+      const calcOffsetWidth = carouselRef.current.offsetWidth - 
+        ((carouselRef.current.childNodes[0].clientWidth * visibleItems) + 
+        ((getMarginRight + getMarginLeft) * visibleItems))
+
+      carouselRef.current.scrollLeft = 
+        (carouselRef.current.offsetWidth - calcOffsetWidth) * currentIndex
+    }
+  }, [currentIndex, carouselRef])
+
+  return { moveNext, movePrev, isDisabled, currentIndex }
 }

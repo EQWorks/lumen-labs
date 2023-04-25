e18e1d111e9709bb4aa84cde4bb6199e6a4a7a30
Hamaad Chughtai <Hamaad102@gmail.com>
2023-04-25 02:55:39 -0400

    accordion - added detailed panel; easily customizable and allows user to pass more than text

---
+++ src/components/accordion/detailed-panel.js
@@ -0,0 +1,111 @@
+import React, { useMemo, forwardRef, useCallback, useState } from 'react'
+import { useResizeDetector } from 'react-resize-detector'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+
+
+const DetailedPanel = forwardRef(({
+  children,
+  classes,
+  id,
+  open,
+  header,
+  ExpandIcon,
+  CompressIcon,
+  setOpen,
+  onChange,
+}, ref) => {
+  const detailedPanelClasses = Object.freeze({
+    detailedRoot: `detailedPanel__root-container ${classes.detailedRoot} shadow-light-50`,
+    detailedInfo: `detailedPanel__info-container ${classes.detailedInfo} cursor-pointer flex items-center`,
+    iconRoot: `detailedPanel__icon-container ${classes.iconRoot} transition-transform duration-300 ease-in-out origin-center transform h-5`,
+    details: `detailedPanel__details-container ${classes.details} transition-opacity ease-in-out duration-300`,
+  })
+
+  const Icon = open.includes(id) ? CompressIcon : ExpandIcon ?? CompressIcon
+  const renderIcon = () => {
+    if (Icon) {
+      return (
+        <Icon className={clsx(detailedPanelClasses.iconRoot, {
+          'rotate-0': open.includes(id),
+          '-rotate-90': !open.includes(id) && !CompressIcon,
+        })} />
+      )
+    }
+    return null
+  }
+
+  const handleClick = () => setOpen((prev) => {
+    if (prev.includes(id)) {
+      prev.splice(prev.indexOf(id), 1)
+      onChange(id, prev)
+      return [...prev]
+    }
+    onChange(id, [...prev, id])
+    return [...prev, id]
+  })
+
+  const [headerHeight, setHeaderHeight] = useState()
+  const headerRef = useCallback((node) => setHeaderHeight(node?.getBoundingClientRect().height), [])
+  const { height: detailsHeight, ref: detailsRef } = useResizeDetector()
+  const height = useMemo(() => headerHeight + detailsHeight, [headerHeight, detailsHeight])
+
+  return (
+    <div
+      ref={ref}
+      style={height ? { height } : {}}
+      className={detailedPanelClasses.detailedRoot}
+    >
+      <div
+        ref={headerRef}
+        onClick={handleClick}
+        className={detailedPanelClasses.detailedInfo}
+      >
+        {header}
+        {renderIcon()}
+      </div>
+      <div
+        ref={detailsRef}
+        className={clsx(detailedPanelClasses.details, {
+          ['max-h-full opacity-100']: open.includes(id),
+          ['max-h-0 opacity-0 overflow-hidden']: !open.includes(id),
+        })}>
+        {children}
+      </div>
+    </div>
+  )
+})
+
+DetailedPanel.propTypes = {
+  children: PropTypes.any.isRequired,
+  classes: PropTypes.objectOf(PropTypes.string),
+  header: PropTypes.elementType,
+  ExpandIcon: PropTypes.elementType,
+  CompressIcon: PropTypes.elementType,
+  id: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
+  open: PropTypes.array,
+  setOpen: PropTypes.func,
+  onChange: PropTypes.func,
+}
+
+DetailedPanel.defaultProps = {
+  open: [],
+  classes: {
+    detailedRoot: '',
+    detailedInfo: '',
+    iconRoot: '',
+    details: '',
+  },
+  header: null,
+  ExpandIcon: null,
+  CompressIcon: null,
+  setOpen: () => {},
+  onChange: () => {},
+}
+
+DetailedPanel.displayName = 'DetailedPanel'
+
+export default DetailedPanel
